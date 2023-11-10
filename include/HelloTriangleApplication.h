a95a50e2b0b8872dd5e3f45934e50a30ce5837ff
AnthonyK213 <tjjzqwh@163.com>
2023-11-10 14:10:03 +0800

    mipmaps

--- include/HelloTriangleApplication.h
+++ include/HelloTriangleApplication.h
@@ -134,23 +134,25 @@ private:
 
   void updateUniformBuffer(uint32_t currentImage);
 
-  void createImage(uint32_t width, uint32_t height, VkFormat format,
-                   VkImageTiling tiling, VkImageUsageFlags usage,
-                   VkMemoryPropertyFlags properties, VkImage &image,
-                   VkDeviceMemory &imageMemory);
+  void createImage(uint32_t width, uint32_t height, uint32_t mipLevels,
+                   VkFormat format, VkImageTiling tiling,
+                   VkImageUsageFlags usage, VkMemoryPropertyFlags properties,
+                   VkImage &image, VkDeviceMemory &imageMemory);
 
   VkCommandBuffer beginSingleTimeCommands();
 
   void endSingleTimeCommands(VkCommandBuffer commandBuffer);
 
   void transitionImageLayout(VkImage image, VkFormat format,
-                             VkImageLayout oldLayout, VkImageLayout newLayout);
+                             VkImageLayout oldLayout, VkImageLayout newLayout,
+                             uint32_t mipLevels);
 
   void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width,
                          uint32_t height);
 
   VkImageView createImageView(VkImage image, VkFormat format,
-                              VkImageAspectFlags aspectFlags);
+                              VkImageAspectFlags aspectFlags,
+                              uint32_t mipLevels);
 
   VkFormat findSupportedFormat(const std::vector<VkFormat> &candidates,
                                VkImageTiling tiling,
@@ -159,6 +161,9 @@ private:
   VkFormat findDepthFormat();
 
   void loadModel();
+
+  void generateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth,
+                       int32_t texHeight, uint32_t mipLevels);
 
 public:
   ~HelloTriangleApplication();
@@ -212,6 +217,7 @@ private:
   VkDescriptorPool descriptorPool;
   std::vector<VkDescriptorSet> descriptorSets;
 
+  uint32_t mipLevels;
   VkImage textureImage;
   VkDeviceMemory textureImageMemory;
 
